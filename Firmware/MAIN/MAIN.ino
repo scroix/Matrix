04a8c41f615d465de4707d764129609dec37433e
maurinElectroTextile <maurin@etextile.org>
2019-12-10 18:58:40 +0100

    Added V1.2 version - working prototype

--- Firmware/MAIN/MAIN.ino
+++ Firmware/MAIN/MAIN.ino
@@ -1,6 +1,6 @@
 /*
-  ** E256 Firmware v1.1 ** (DO NOT WORK!)
-  This file is part of the eTextile-matrix-sensor project - http://matrix.eTextile.org
+  ** E256 Firmware V1.2
+  This file is part of the eTextile-matrix-sensor project : http://matrix.eTextile.org
   Copyright (c) 2014-2019 Maurin Donneaud <maurin@etextile.org>
   This work is licensed under Creative Commons Attribution-ShareAlike 4.0 International license, see the LICENSE file for details.
 */
